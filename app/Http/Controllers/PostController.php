fd8869a2f99038b85eab4dbabfa617cbedcf8541
AriefWahyudii <ariefwahyudi.id@gmail.com>
2023-10-20 10:14:43 +0700

    show comments data in PostResouerce

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -44,7 +44,7 @@ class PostController extends Controller
         $post = Post::find($id);
         $post->update($request->all());
 
-        return new PostResource($post->loadMissing('writer'));
+        return new PostResource($post->loadMissing('writer', 'comments'));
     }
 
     function delete($id) {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -20,5 +21,15 @@ class Post extends Model
     public function writer(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get all of the comments for the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
